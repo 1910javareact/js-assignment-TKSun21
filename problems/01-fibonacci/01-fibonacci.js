88e8ce8af0caaad472b438ea859260f100ae2031
TKSun21 <kietnguyen2016@gmail.com>
2019-11-16 16:47:43 -0500

    1,2, and 4

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -3,4 +3,19 @@ Define function: fib(n)
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
 
+    let x = n;
+        if (x === 0) {
+            return 0;
+        }
+        else if (x === 1){
+            return 1;
+        }
+        else if (x < 0) {
+            console.log("Can't be negative")
+        }
+        else {
+            return fib(x-1) + fib(x-2);
+        }
 }
+
+console.log(fib(8))
--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -3,5 +3,18 @@ Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
 function bubbleSort(numArray) {
-  
+
+    let x = numArray.length
+
+    for (let i = 0; i < x; i++)
+        for (let j = 0; j < x; j++){
+            if (numArray[j] > numArray[j + 1]){
+                let temp = numArray[j]
+                numArray[j] = numArray[j + 1]
+                numArray[j + 1] = temp
+            }
+        }
+    return numArray
 }
+
+console.log(bubbleSort([3,2,1,5,7,8,6,4]))
