86133a69a9553c80660d2d6d092615a35a126fe9
TKSun21 <kietnguyen2016@gmail.com>
2019-11-16 22:48:27 -0500

    3 and 5

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,13 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+
+    let wordRes = ""
+
+    for(let i = someStr.length - 1; i >= 0; i--){
+        wordRes += someStr[i]
+    }
+    return wordRes
 }
+
+console.log(reverseStr("Infinite8"))
--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -4,5 +4,12 @@ Return the substring contained between startIndex (inclusive) and endIndex (excl
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
+    
+    console.log(someStr.substring(startIndex,endIndex))
 
+    if (endIndex < startIndex){
+        throw "Incorrect input is entered"
+    }
 }
+
+console.log(substring("YEET 2035", 5, 9))
